bc04293090c2279b9c24b5ccd1b10678a3036602
Hugo Ye <hugoye122@gmail.com>
2025-09-10 16:29:33 -0400

    did workaaaaa

---
+++ src/lab1/Reduce.java
@@ -0,0 +1,21 @@
+package lab1;
+
+public class Reduce {
+    public static void main(String[] args) {
+
+    }
+
+    public static int reduce(int n) {
+        int i = 0;
+        while (n > 0) {
+            if (n % 2 == 0) {
+                n = n /2;
+            } else {
+                n = n - 1;
+            }
+            i++;
+
+        }
+        return i;
+    }
+}
