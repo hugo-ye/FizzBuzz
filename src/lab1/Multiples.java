752913ce0e760805000524d2ac3a15f845983e85
Hugo Ye <hugoye122@gmail.com>
2025-09-10 16:19:08 -0400

    did workaaaaa

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -15,4 +15,15 @@ public class Multiples {
         }
         return count;
     }
+
+    public static int multiples() {
+        int count = 0;
+        for (int i = 1; i < 1000; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                count++;
+            }
+
+        }
+        return count;
+    }
 }
